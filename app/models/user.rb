b350d6cf43700dc33293c19f5d99b323366d4357
liujun <lj90214@gmail.com>
2015-08-04 10:34:39 +0800

    add devise for use model management
    generate user model and devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,65 +1,50 @@
-require 'digest'
-
 class User
   include Mongoid::Document
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, 
+	       :validatable
+
+  ## Mobile authenticatable
+  field :mobile,             type: String, default: ""
+
+  ## Database authenticatable
+  field :email,              type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+
+  ## Recoverable
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+
+  ## Rememberable
+  field :remember_created_at, type: Time
+
+  ## Trackable
+  field :sign_in_count,      type: Integer, default: 0
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at,    type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip,    type: String
+
+  ## Confirmable
+  # field :confirmation_token,   type: String
+  # field :confirmed_at,         type: Time
+  # field :confirmation_sent_at, type: Time
+  # field :unconfirmed_email,    type: String # Only if using reconfirmable
+
+  ## Lockable
+  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
+  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
+  # field :locked_at,       type: Time
   include Mongoid::Timestamps
 
-  # 基础信息
-  field :username, type: String
-  field :password_hash, type: String
-  field :email, type: String
-  field :mobile, type: String
-
-  # 激活验证
-  field :activate_token, type: String
-  field :active_token_at, type: DateTime
-  field :is_activated, type: Boolean
-
-  # 忘记密码
-  field :pass_reset_token, type: String
-  field :pass_reset_token_at, type: DateTime
-  field :pass_reset_at, type: DateTime
-
-  # 跟踪登录信息
-  field :last_login_at, type: DateTime
-  field :last_login_ip, type: String
-  field :current_login_at, type: DateTime
-  field :current_login_ip, type: String
-
-  # 是否是超级管理员
-  field :is_super, type: Boolean
-
-  # 用户是否被禁用
-  field :is_blocked, type: Boolean
-
-  # 用户角色
-  has_and_belongs_to_many :roles
-
-  # 验证
-  validates :username, :email, :password, presence: true
-  validates :username, uniqueness: true
-  validates :password, confirmation: true
-
-  def activated?
-    self.is_activated == 1
+  def self.find_or_create_by_mobile(mobile)
+    find_or_create(mobile: mobile)
   end
 
-  def password=(password)
-    self.password_hash = Digest::MD5.hexdigest(password || "")
+  def admin?
+    Setting.admin_mobiles.include?(self.mobile)
   end
-
-  def reset_password(password)
-    self.password = password
-  end
-
-  def has_role?(role)
-  end
-
-  def can? 
-  end
-
-  def super?
-    self.is_super
-  end
-
+  
 end
